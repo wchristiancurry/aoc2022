0a22a19b5f3898e2bf6f176e043418862c9666d1
Christian Curry <christian.curry@slalom.com>
2022-11-30 23:56:55 -0500

    first commit

---
+++ template.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const { performance } = require('perf_hooks');
+
+function getInput() {
+  try {
+    input = fs.readFileSync('./in.txt', 'utf8');
+    // input = input.split("\n");
+  } catch (err) {
+    console.error(err);
+  }
+
+  return input;
+}
+
+function problem1(input) { 
+}
+
+function problem2(input) { 
+}
+
+function solve() {
+  var input = getInput();
+
+  let startTime = performance.now();
+  console.log('problem 1 solution: ' + problem1(input));
+  console.log('problem 1 execution time: ' + (performance.now() - startTime) + ' ms');
+
+  startTime = performance.now();
+  console.log('problem 2 solution: ' + problem2(input));
+  console.log('problem 2 execution time: ' + (performance.now() - startTime) + ' ms');
+}
+
+solve();
